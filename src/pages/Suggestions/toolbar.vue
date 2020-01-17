0623882d99155c37e3afc6a494be77a1babc8bb7
mailydina <mailydina@gmail.com>
2020-01-17 16:50:10 +0700

    tugas akhir

---
+++ src/pages/Suggestions/toolbar.vue
@@ -0,0 +1,41 @@
+<template>
+  <q-toolbar>
+    <q-btn
+      flat
+      dense
+      round
+      @click="toggleLeftDrawer"
+      aria-label="Menu"
+    >
+      <q-icon name="menu" />
+    </q-btn>
+      <editor-menu-bar :editor="editor">
+        <div class="menubar" slot-scope="{ commands }">
+          <button class="menubar__button" @click="commands.mention({ id: 1, label: 'Philipp Kühn' })">
+            <svgicon name="mention" />
+            <span>Insert Mention</span>
+          </button>
+        </div>
+      </editor-menu-bar>
+
+  </q-toolbar>
+</template>
+
+<script>
+import UiMixin from 'src/mixins/ui'
+import { EditorMenuBar } from 'tiptap'
+import { mapActions } from 'vuex'
+export default {
+  name: 'DefaultToolbar',
+  mixins: [ UiMixin ],
+  props: [],
+  components: {
+    EditorMenuBar
+  },
+  methods: {
+    ...mapActions([
+      'toggleLeftDrawer'
+    ])
+  }
+}
+</script>
